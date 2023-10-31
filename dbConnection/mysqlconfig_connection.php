57708f4f5bdcca2ec8f1a1885007ae45fd3070ca
davem4tth3w <davemmatthew0807@gmail.com>
2023-10-31 14:56:09 +0800

    Update mysqlconfig_connection.php

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -7,5 +7,6 @@
     $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
     OR die('Could not connect to MySQL: ' . mysqli_connect_error());
     // OR dies('Could not connect to MySQL: ' . mysqli_connect_error());
+    // comment
 
 ?>
